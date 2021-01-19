521a8c0eacf371fcca8e93ea73d343c60c20d0d1
Damyan <damo.ts.04@gmail.com>
2021-01-19 19:53:28 +0200

    adding latest changes and scss files

--- BeachTowelShop-App/Controllers/EmailController.cs
+++ BeachTowelShop-App/Controllers/EmailController.cs
@@ -19,8 +19,11 @@ namespace BeachTowelShop_App.Controllers
         [HttpPost]
         public IActionResult SendDiscounts(string email)
         {
-           
-             _emailService.SendEmail(email,"Discount","Hey,here is your discount");
+            if (email != null)
+            {
+                _emailService.SendEmail(email, "Discount", "Hey,here is your discount");
+            }
+             
             return RedirectToAction("Index","Home");
         }
        
